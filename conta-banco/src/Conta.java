dff8c697688477c2799e9b9238632aac0f8d33c8
Rubens Parente <rubensparente@yahoo.com.br>
2023-08-27 11:38:53 -0300

    meu projeto conta banco

---
+++ conta-banco/src/Conta.java
@@ -0,0 +1,36 @@
+public class Conta {
+    private String nomeCliente;
+    private int numeroConta;
+    private String agencia;
+    private double saldo;
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+    public String getAgencia() {
+        return agencia;
+    }
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void imprimir(){
+        System.out.println("Olá " +getNomeCliente()+", obrigado por criar uma conta em nosso banco, sua agênceia é "+getAgencia()+", conta " +getNumeroConta()+ " e seu saldo " +getSaldo()+ " já está disponível para saque." );
+    }
+    
+}
---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Scanner; //Clase scanner importada
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+       //TODO: Conhecer e importar a classe Scanner
+       //Exibir as mensagens para o nosso usuário
+       //Obter pelo scanner os valores digitados no terminal
+       //Ixibir a mensagem da conta criada
+
+       Conta conta = new Conta();
+
+       Scanner entrada = new Scanner(System.in); //Criar objeto Scanner
+
+       System.out.println("Por favor, digite o número da Agência!");
+       conta.setAgencia(entrada.nextLine());
+
+       System.out.println("Digite a conta: ");
+       conta.setNumeroConta(entrada.nextInt());
+
+       System.out.println("Digite seu nome: ");
+       conta.setNomeCliente(entrada.next());
+
+       System.out.println("Valor para deposito: ");
+       conta.setSaldo(entrada.nextDouble());
+
+       conta.imprimir();    
+
+    }
+}
